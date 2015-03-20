304e5af06d05d2ef4f8d54f80a1eb9e314034ca3
Tan Theng Lee <thengleetan@gmail.com>
2015-03-20 14:08:12 +0800

    Retrieve mongodb document to render in view

--- app.js
+++ app.js
@@ -1,18 +1,35 @@
 var express = require('express'),
 	app = express(),
-	cons = require('consolidate'); //Wrappers for template libraries for Express
+	cons = require('consolidate'), //Wrappers for template libraries for Express
+	MongoClient = require('mongodb').MongoClient,
+	Server = require('mongodb').Server;
 
 app.engine('html', cons.swig); //Swig template engine integrated with Express using consolidate
 app.set('view engine', 'html'); //Set view engine for express to be html
 app.set('views', __dirname + '/views'); //Set directory to look for views
 
+//Create object to set up connection information
+var mongoclient = new MongoClient(new Server('localhost', 27017,
+												{ 'native_parser' : true }));
+//Set reference to db, no connection yet
+var db = mongoclient.db('demo');
+
+
+
 app.get('/', function(req, res){
-	res.render('hello', { 'name': 'Swig' }); //Look for hello.html page and pass in value for 'name'
+	db.collection('hello_mongo_express').findOne({}, function(err, doc){
+		if (err) throw err;
+		res.render('hello', doc); //Look for hello.html page and pass in document
+	});
 });
 
 app.get('*', function(req, res){
 	res.send("Page not found", 404);
 });
 
-app.listen(7000);
-console.log("Express server started on port 7000");
+//Start listening when the db connection is open
+mongoclient.open(function(err, mongoclient){
+	if (err) throw err;
+	app.listen(7000);
+	console.log("Express server started on port 7000");
+});
