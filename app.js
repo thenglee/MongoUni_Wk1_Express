b482605e7762a6cb5d005db03607878e4f51cf4d
Tan Theng Lee <thengleetan@gmail.com>
2015-03-20 13:15:49 +0800

    Add wildcard route to provide page not found status

--- app.js
+++ app.js
@@ -5,5 +5,9 @@ app.get('/', function(req, res){
 	res.send("Hello, World!");
 });
 
+app.get('*', function(req, res){
+	res.send("Page not found", 404);
+});
+
 app.listen(8080);
 console.log("Express server started on port 8080");
